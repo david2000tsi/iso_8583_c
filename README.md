9866116501ebd0fa9414058abd748bc40ab8dfa1
David Alves <david2000tsi@gmail.com>
2018-12-12 12:44:29 -0200

    First commit - Added readme and app file

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# ISO8583 C Library
+
+This is a basic implementation of ISO8583 for C language for generate and decode messages.
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	printf("This is a basic implementation of ISO8583 for C language\n");
+	return 0;
+}
