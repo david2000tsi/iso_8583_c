a6c73858779098eee1ff0348b6785bfd1aada74d
David Alves <david2000tsi@gmail.com>
2018-12-13 22:41:19 -0200

    Implemented decode_message and other needed functions and added some improvements

--- inc/iso_8583.h
+++ inc/iso_8583.h
@@ -9,11 +9,23 @@
 int init(int iso_version);
 
 /**
+ * @brief Release all internal memory allocated by other functions.
+ */
+void release();
+
+/**
  * @brief Set message mti.
  * @param[in] mti The message mti.
  * @return Returns 0 to success or -1 case error.
  */
 int set_mti(const char *mti);
+
+/**
+ * @brief Get message mti.
+ * @param[out] mti The buffer to store the message mti.
+ * @return Returns 0 to success or -1 case error.
+ */
+int get_mti(char *mti);
 
 /**
  * @brief Add field in iso message.
@@ -25,6 +37,14 @@ int set_mti(const char *mti);
 int add_field(int field, const char *data, int length);
 
 /**
+ * @brief Retrieve a value of field.
+ * @param[in] field The field number.
+ * @param[out] data The buffer to store field value.
+ * @return Returns 0 if field was recovered or -1 case error.
+ */
+int get_field(int field, char *data);
+
+/**
  * @brief Remove field from iso message.
  * @param[in] field The field number.
  * @return Returns 0 if field was removed or -1 case there is no field number set.
@@ -32,15 +52,17 @@ int add_field(int field, const char *data, int length);
 int remove_field(int field);
 
 /**
- * @brief Cleans all internal variables.
- */
-void clear();
-
-/**
  * @brief Generate iso message according added fields.
  * @param[out] message The buffer where the message will be stored.
  * @return Returns 0 to success or -1 case error.
  */
 int generate_message(char *message);
+
+/**
+ * @brief Decode iso message and fill internal fields.
+ * @param[in] message The message to be decoded.
+ * @return Returns 0 to success or -1 case error.
+ */
+int decode_message(const char *message);
 
 #endif
